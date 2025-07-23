07a051f854a71b57b433f03b600f5adb50c1da22
Guray Cha <guraycha@gmail.com>
2025-07-23 23:23:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/shoppe-lane/', 
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true' 
+    ? '/shoppe-lane/' 
+    : '/',
 });
