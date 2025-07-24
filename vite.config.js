065d3d03bc5e78b1d4becb49967c34c365bf8330
Guray Cha <guraycha@gmail.com>
2025-07-24 20:20:09 +0800

    . . .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-const basePath = isGitHubPages ? '/shoppe-lane/' : '/';
-
 export default defineConfig({
   plugins: [react()],
-  base: basePath,
+  base: '/', // ALWAYS root for Vercel
 });
