83f07225c95fbf2dea2c13bfc142e694d0d22358
Guray Cha <guraycha@gmail.com>
2025-07-24 20:04:02 +0800

    . . .

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 const isGithubPages = import.meta.env.BASE_URL === '/shoppe-lane/';
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const basePath = isGitHubPages ? '/shoppe-lane/' : '/';
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true' 
-    ? '/shoppe-lane/' 
-    : '/',
+  base: basePath,
 });
